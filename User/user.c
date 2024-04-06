de31e7f4a55e7e5ebb961b7e39d9b0bdef017751
seekskywang <seekskywang@gmail.com>
2024-04-06 10:49:30 +0800

    1

--- User/user.c
+++ User/user.c
@@ -4425,7 +4425,7 @@ void Disp_Sys_Item(void)
         
 	}
 	Colour.Fword=LCD_COLOR_GREY;
-	WriteString_16(LIST2+90, FIRSTLINE+SPACE1*6, "SoftVer :3.2",  0);
+	WriteString_16(LIST2+90, FIRSTLINE+SPACE1*6, "SoftVer :3.3",  0);
 	//2.5增加标准RTU协议选择
 	//2.6上位机通讯改到前面板
 	//2.7仪器出厂参数可以自定义设置
@@ -4434,6 +4434,7 @@ void Disp_Sys_Item(void)
 	//3.0修正关机设置点参数不保存bug
 	//3.1读取时间增加到40ms一次，修正编码器设置bug（&& flag_spin == 0）
 	//3.2增加设置电流时的功率限制判断
+	//3.3修正按键开关bug
 	Hex_Format(DispValue.version,1,2,0);
 	WriteString_16(LIST2+90, FIRSTLINE+SPACE1*7, "BoardVer:",  0);
 	WriteString_16(LIST2+90+90, FIRSTLINE+SPACE1*7, DispBuf,  0);
